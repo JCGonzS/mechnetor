9fd33e2c12082799ead8e79a8310bfd3b5b69857
JCGonzS <juancarlosgonzs@gmail.com>
2019-03-26 15:33:37 +0100

    Updated data files. Fixed gene/accession mapping. Added source field to PTMs

--- static/data/make_protein_data_json.py
+++ static/data/make_protein_data_json.py
@@ -21,18 +21,18 @@ from Bio import SwissProt
 ## Data Files
 data_dir = ""
 common_data_dir = data_dir+"common/"
-pfam_data_file = common_data_dir + "Pfam-A.hmm_r32.0.dat.gz"
+pfam_data_file = common_data_dir + "Pfam-A.hmm.dat.gz"
 species = "Hsa"
 sp_data_dir = data_dir+"species/"+species+"/"
-proteome_file =     sp_data_dir + "uniprot_sprot_human_complete_20303prts_March_2018.fasta.gz"
-uniprot_text_file = sp_data_dir + "uniprot_homo_sapiens_proteome_73112prts_Aug2018_data.txt.gz"
-pfam_matches_file = sp_data_dir + "pfamA_matches_9606.tsv.gz"
+# uniprot_text_file = sp_data_dir + "uniprot_homo_sapiens_proteome_73112prts_Aug2018_data.txt.gz"
+uniprot_text_file = sp_data_dir + "uniprot_homo_sapiens_proteome_73928prts_Mar2019_data.txt.gz"
+pfam_matches_file = sp_data_dir + "pfamA_matches_9606_Aug18.tsv.gz"
 elm_hits_file =     sp_data_dir + "elm_parsed_info_noOverlap.tsv.gz"
-psp_file =          sp_data_dir + "PSP_ptms_human.tsv.gz"
+psp_file =          sp_data_dir + "PSP_ptms_human_Mar2019.tsv.gz"
 
 
 def open_file(input_file, mode="r"):
-    """ Open file Zipped or not
+    """ Open file zipped or not
     """
     if re.search(".gz$", input_file):
         infile = gzip.open(input_file, mode)
@@ -49,68 +49,126 @@ def assign_Number(inputString):
 	else:
 		return inputString
 
-def get_protein_data_from_uniprot_text(uniprot_file):
-    """From SwissProt"""
+def merge_gene_names(genes):
+    """For a list of similar gene names ("GENE1A", "GENE2B", "GENE3C"),
+    returns a string following the format: "GENE(1A,2B,3C)"
+    """
+    pattern = ""
+    flag = 0
+    for i in range(1, len(genes[0])+1):
+        reg = genes[0][:i]
+        for g in genes[1:]:
+            if reg != g[:i]:
+                flag = 1
+        if flag == 0:
+            pattern = reg
+        else:
+            break
+    var = []
+    for g in genes:
+        v = g.split(pattern)[1]
+        if v.strip():
+            var.append(v)
 
-    D = defaultdict(dict)
-    D["doms"] = defaultdict(dict)
+    return pattern+"("+",".join(var)+")"
+
+
+def get_protein_data_from_uniprot_text(uniprot_file):
+    """Extracts information from a UniProt text file.
+    Requires Bio::SwissProt module
+    """
+
+    D = defaultdict(lambda: defaultdict(set))
+    # D["doms"] = defaultdict(dict)
     for record in SwissProt.parse(open_file(uniprot_file)):
-        dc = record.data_class
-        # if dc != "Reviewed":
-        #     continue
+        dc  = record.data_class
         uni_id = record.entry_name
         accs = record.accessions
         uni_ac = accs[0].upper()
-        gn = record.gene_name
-        if record.gene_name != "":
-            gn = re.search("Name[s]?=([^;]+)", record.gene_name ).group(1).split()[0]
-        des = re.search("Name: Full=([^;]+)", record.description).group(1)
-        seq = record.sequence
-        length = record.sequence_length
+        des = re.search("Name: Full=([^;|{]+)", record.description).group(1)
+        gns, syns = [uni_id], []
+        if record.gene_name.strip():
+            names = [match.split()[0] for match in re.findall("Name=([^;]+)", record.gene_name)]
+            if len(names)>0:
+                gns = []
+                for name in names:
+                    name = name.split("_")[0]
+                    if name not in gns:
+                        gns.append(name)
 
-        D["AC"][uni_ac], D["AC"][uni_id], D["AC"][gn] = uni_ac.upper(), uni_ac.upper(), uni_ac.upper()
-        D["AC"][uni_ac.upper()], D["AC"][uni_id.upper()], D["AC"][gn.upper()] = uni_ac.upper(), uni_ac.upper(), uni_ac.upper()
-        D["ID"][uni_ac], D["ID"][uni_id], D["ID"][gn] = uni_id.upper(), uni_id.upper(), uni_id.upper()
-        D["ID"][uni_ac.upper()], D["ID"][uni_id.upper()], D["ID"][gn.upper()] = uni_id.upper(), uni_id.upper(), uni_id.upper()
-        D["GN"][uni_ac], D["GN"][uni_id], D["GN"][gn] = gn.upper(), gn.upper(), gn.upper()
-        D["GN"][uni_ac.upper()], D["GN"][uni_id.upper()], D["GN"][gn.upper()] = gn.upper(), gn.upper(), gn.upper()
-        D["seq"][uni_ac.upper()] = seq
-        D["dc"][uni_ac.upper()] = dc
+            syns = [match.split()[0].strip(",") for match in re.findall("Synonyms=([^;]+)", record.gene_name)]
+
+        if len(gns)>1:
+            gn = merge_gene_names(gns)
+        else:
+            gn = gns[0]
+
+        # if dc != "Reviewed":
+        #     continue
+        # if gn in D["AC"]:
+        #     print uni_ac, gn, D["AC"][gn]
+        # else:
+        #     D["AC"][gn] = uni_ac.upper()
+        for dic, val in zip(["AC", "ID", "GN"], [uni_ac, uni_id, gn]):
+            for key in [uni_ac, uni_id, gn]:
+                D[dic][key].add(val.upper())
+                D[dic][key.upper()].add(val.upper())
+
         D["des"][uni_ac.upper()] = des
+        D["dc"][uni_ac.upper()] = dc
+        D["seq"][uni_ac.upper()] = record.sequence
 
-        for ac in accs:
-            D["AC"][ac] = uni_ac.upper()
-            D["AC"][ac.upper()] = uni_ac.upper()
+        for ac in accs[1:]:
+            # if ac!=uni_ac and ac in D["AC"]:
+            #     print uni_ac, ac, D["AC"][ac]
+            D["AC"][ac].add(uni_ac.upper())
+            D["AC"][ac.upper()].add(uni_ac.upper())
+        for g in gns[1:]:
+            D["AC"][g].add(uni_ac.upper())
+            D["AC"][g.upper()].add(uni_ac.upper())
 
-        for feat in record.features:
-            if feat[0] == "DOMAIN":
-                name = feat[-2].split(".")[0]
-                if re.search("([\d]+)", str(feat[1])) and re.search("([\d]+)", str(feat[2])):
-                    start = int(re.search("([\d]+)", str(feat[1])).group(1))
-                    end = int(re.search("([\d]+)", str(feat[2])).group(1))
-                    D["doms"][uni_ac][(start,end)] = name
+        # for feat in record.features:
+        #     if feat[0] == "DOMAIN":
+        #         name = feat[-2].split(".")[0]
+        #         if re.search("([\d]+)", str(feat[1])) and re.search("([\d]+)", str(feat[2])):
+        #             start = int(re.search("([\d]+)", str(feat[1])).group(1))
+        #             end = int(re.search("([\d]+)", str(feat[2])).group(1))
+        #             D["doms"][uni_ac][(start,end)] = name
+
     return D
 
-def get_pfam_doms(pfam_file, prot_id, max_eval=999):
+def get_pfam_doms(pfam_file, prot_dict, max_eval=999):
     """Pfam-A matches in species proteome. File downloaded from PFAM.
     """
     pfams = defaultdict(lambda: defaultdict(set) )
+    pfams_temp = defaultdict(lambda: defaultdict(set) )
     pfam_sets = defaultdict(set)
     pfam_names = {}
-
+    d = {}
     with open_file(pfam_file) as f:
         for line in f:
             if line[0] != "#":
                 t = line.rstrip().split("\t")
                 uni_ac = t[0].upper()
-                start, end = int(t[3]), int(t[4])
-                pfam_ac, pfam_name, domain_e_val = t[5], t[6], float(t[12])
+                start, end = int(t[4]), int(t[5])
+                pfam_ac, pfam_name, domain_e_val = t[6], t[7], float(t[13])
 
-                if uni_ac in prot_id:# and domain_e_val <= max_eval:
-                    uni_ac = prot_id[uni_ac]
+                # if domain_e_val <= max_eval: ## No e-value cut-off. Just take what is annotated by Pfam
+
+                if uni_ac in prot_dict["seq"]: ## Keep annotation for primary UniProt accessions
                     pfams[uni_ac][pfam_ac].add((start, end, domain_e_val))
                     pfam_sets[pfam_ac].add(uni_ac)
                     pfam_names[pfam_ac] = pfam_name
+                elif uni_ac in prot_dict["AC"]: ## Save annotation for secondary accessions in a different dictionary
+                    pfams_temp[uni_ac][pfam_ac].add((start, end, domain_e_val))
+
+    ## Transfer annotation to those main accessions which were not annotated
+    for alt_ac in pfams_temp:
+        for uni_ac in prot_dict["AC"][alt_ac]:
+            if uni_ac not in pfams: ## If the associated main accession is not annotated already:
+                pfams[uni_ac] = pfams_temp[alt_ac]
+                pfam_sets[pfam_ac].add(uni_ac)
+                pfam_names[pfam_ac] = pfam_name
 
     return pfams, pfam_sets, pfam_names
 
@@ -148,14 +206,16 @@ def pfam_descriptions(pfam_data_file):
                 d[ac] = des
     return d
 
-def get_linear_motifs(elm_hits_file, prot_id, max_eval=1):
+def get_linear_motifs(elm_hits_file, prot_dict, max_eval=1):
     """ Reads pre-generated file with ELMs annotated for each protein
         If there's any filtering for these, it has already been done. All the
         info of this file is considered equally valid (take all)
     """
     elms = defaultdict(lambda: defaultdict(set) )
+    elms_temp = defaultdict(lambda: defaultdict(set) )
     elm_sets = defaultdict(set)
     elm_names = {}
+    s = set()
     with open_file(elm_hits_file) as f:
         for line in f:
             if line[0] != "#" and line.strip():
@@ -165,40 +225,60 @@ def get_linear_motifs(elm_hits_file, prot_id, max_eval=1):
                 elm_start, elm_end = int(t[2]), int(t[3])
                 some_score = float(t[4])
 
-                if some_score <= max_eval and uni_ac in prot_id:
-                    uni_ac = prot_id[uni_ac]
-                    elms[uni_ac][elm_ac].add((elm_start, elm_end, some_score))
-                    elm_sets[elm_ac].add(uni_ac)
-                    elm_names[elm_ac] = elm_name
+                if some_score <= max_eval:
+                    if uni_ac in prot_dict["seq"]:
+                        elms[uni_ac][elm_ac].add((elm_start, elm_end, some_score))
+                        elm_sets[elm_ac].add(uni_ac)
+                        elm_names[elm_ac] = elm_name
+                    elif uni_ac in prot_dict["AC"]:
+                        elms_temp[uni_ac][elm_ac].add((elm_start, elm_end, some_score))
+
+    for alt_ac in elms_temp:
+        for uni_ac in prot_dict["AC"][alt_ac]:
+            if uni_ac not in elms:
+                elms[uni_ac] = elms_temp[alt_ac]
+                elm_sets[elm_ac].add(uni_ac)
+                elm_names[elm_ac] = elm_name
     return elms, elm_sets, elm_names
 
 def get_ptms(psp_file, prot_id):
     psp = defaultdict(set)
-    done_pos = defaultdict(set)
+    psp_temp = defaultdict(set)
     with open_file(psp_file) as f:
         for line in f:
             t = line.rstrip().split("\t")
+            pos, mod = t[4].split("-")
+            res, pos = re.search("(\w)([\d]+)", pos).group(1,2)
+            lt_lit="."
+            ms_lit="."
+            ms_cst="."
+            cst_cat="."
             uni_ac = t[2]
-            if uni_ac in prot_id:
-                uni_ac = prot_id[uni_ac]
-                pos, mod = t[4].split("-")
-                res, pos = re.search("(\w)([\d]+)", pos).group(1,2)
-                lt_lit="."
-                ms_lit="."
-                ms_cst="."
-                cst_cat="."
-                if len(t)>10 and hasNumbers(t[10]):
-                    lt_lit = assign_Number(t[10])
-                if len(t)>11 and hasNumbers(t[11]):
-                    ms_lit = assign_Number(t[11])
-                if len(t)>12 and hasNumbers(t[12]):
-                    ms_cst = assign_Number(t[12])
-                if len(t)>13 and hasNumbers(t[13]):
-                    cst_cat = assign_Number(t[13])
+            if len(t)>10 and hasNumbers(t[10]):
+                lt_lit = assign_Number(t[10])
+            if len(t)>11 and hasNumbers(t[11]):
+                ms_lit = assign_Number(t[11])
+            if len(t)>12 and hasNumbers(t[12]):
+                ms_cst = assign_Number(t[12])
+            if len(t)>13 and hasNumbers(t[13]):
+                cst_cat = assign_Number(t[13])
 
-                if pos not in done_pos[(uni_ac,mod)]:
+            if uni_ac in prot_id["seq"]:
+                seq = prot_id["seq"][uni_ac]
+                if len(seq)>=int(pos) and seq[int(pos)-1]==res:
                     psp[uni_ac].add( (res, pos, mod) )
-                    done_pos[(uni_ac,mod)].add(pos)
+            elif uni_ac in prot_id["AC"]:
+                psp_temp[uni_ac].add( (res, pos, mod) )
+
+
+    for alt_ac in psp_temp:
+        for uni_ac in prot_dict["AC"][alt_ac]:
+            if uni_ac not in psp:
+                seq = prot_id["seq"][uni_ac]
+                for (res,pos,mod) in psp_temp[alt_ac]:
+                    if len(seq)>=int(pos) and seq[int(pos)-1]==res:
+                        psp[uni_ac].add( (res, pos, mod) )
+
     return psp
 
 def get_ppi_pairs(biogrid_file):
@@ -211,9 +291,10 @@ def get_ppi_pairs(biogrid_file):
 
 	return
 
-biogrid_file = sp_data_dir + "BIOGRID-ORGANISM-3.5.165.tab2.txt.gz"
-get_ppi_pairs(biogrid_file)
-sys.exit
+# biogrid_file = sp_data_dir + "BIOGRID-ORGANISM-3.5.165.tab2.txt.gz"
+# get_ppi_pairs(biogrid_file)
+# sys.exit
+
 ####
 error_msg = """
 ERROR
@@ -231,24 +312,21 @@ if mode not in ["mongo", "normal"]:
 
 
 ## 1. Get Uniprot Accession, IDs and Gene names conversions ###
-# prot_id_dict, sequences = parse_fasta(proteome_file)
 prot_dict = get_protein_data_from_uniprot_text(uniprot_text_file)
 
 ## 2. Get Pfam domains and ELMs
-pfams, pfam_sets, pfam_names = get_pfam_doms(pfam_matches_file,
-                                                prot_dict["AC"])
-
+pfams, pfam_sets, pfam_names = get_pfam_doms(pfam_matches_file, prot_dict)
 pfam_des = pfam_descriptions(pfam_data_file)
+elms, elm_sets, elm_names = get_linear_motifs(elm_hits_file, prot_dict)
 
-elms, elm_sets, elm_names = get_linear_motifs(elm_hits_file,
-                                            prot_dict["AC"])
 ## 3. Get PTMs
-ptms = get_ptms(psp_file, prot_dict["AC"])
+ptms = get_ptms(psp_file, prot_dict)
 
 ## 4. Output
 pp = pprint.PrettyPrinter(indent=4)
-outfile_name = sp_data_dir+"protein_data_"+species+"_"+mode+".json.gz"
-if mode == "normal":
+outfile_name = sp_data_dir+"protein_data_"+species+"_"+mode+"3.json.gz"
+
+if mode == "json":
     protein_data = {}
     for uni_ac in prot_dict["seq"]:
         protein_data[uni_ac] = {
@@ -315,19 +393,20 @@ elif mode == "mongo":
     protein_data = {}
     with open_file(outfile_name, "w") as out:
         for uni_ac in prot_dict["seq"]:
+            seq = prot_dict["seq"][uni_ac]
             protein_data = {
-                    "uniprot_acc" : uni_ac,
-                    "uniprot_id" : prot_dict["ID"][uni_ac],
-                    "gene" : prot_dict["GN"][uni_ac],
-                    "description" : prot_dict["des"][uni_ac],
-                    "data_class" : prot_dict["dc"][uni_ac],
-                    "length" : len(prot_dict["seq"][uni_ac]),
-                    "sequence" : prot_dict["seq"][uni_ac],
-                    "pfams" : [],
-                    "elms" : [],
+                    "uniprot_acc" :     uni_ac,
+                    "uniprot_id" :      list(prot_dict["ID"][uni_ac])[0], # always, only 1
+                    "gene" :            list(prot_dict["GN"][uni_ac])[0],
+                    "description" :     prot_dict["des"][uni_ac],
+                    "data_class" :      prot_dict["dc"][uni_ac],
+                    "length" :          len(seq),
+                    "sequence" :        seq,
+                    "pfams" :           [],
+                    "elms" :            [],
                     "phosphorylation" : [],
-                    "acetylation" : [],
-                    "cosmic_muts" : []
+                    "acetylation" :     [],
+                    "cosmic_muts" :     []
             }
 
             if uni_ac in pfams:
@@ -340,11 +419,11 @@ elif mode == "mongo":
                                                       key=lambda x: int(x[0])):
                         protein_data["pfams"].append(
                             {
-                                "acc" : pfam_ac,
-                                "des": des,
-                                "name" : pfam_name,
+                                "acc" :   pfam_ac,
+                                "des":    des,
+                                "name" :  pfam_name,
                                 "start" : int(start),
-                                "end" : int(end),
+                                "end" :   int(end),
                                 "e-val" : float(e_val)
                             })
 
@@ -355,11 +434,11 @@ elif mode == "mongo":
                                                     key=lambda x: int(x[0])):
                         protein_data["elms"].append(
                             {
-                                "acc" : elm_ac,
-                                "name" : elm_name,
+                                "acc" :   elm_ac,
+                                "name" :  elm_name,
                                 "start" : int(start),
-                                "end" : int(end),
-                                "seq" : prot_dict["seq"][uni_ac][start-1:end],
+                                "end" :   int(end),
+                                "seq" :   seq[start-1:end],
                                 "score" : float(score)
                             })
 
@@ -373,9 +452,27 @@ elif mode == "mongo":
                     protein_data[ptm_type].append(
                         {
                             "pos" : int(pos),
-                            "res" : res
+                            "res" : res,
+                            "source": "PSP"
                         })
+            else:
+                for i in range(9):
+                    iso_uni_ac = uni_ac+"-"+str(i)
+                    if iso_uni_ac in ptms:
+                        for (res, pos, mod) in sorted(ptms[iso_uni_ac],
+                                                     key=lambda x: int(x[1])):
+                            if mod == "p":
+                                ptm_type = "phosphorylation"
+                            elif mod == "ac":
+                                ptm_type = "acetylation"
+                            protein_data[ptm_type].append(
+                                {
+                                    "pos" : int(pos),
+                                    "res" : res,
+                                    "source": "PSP,-"+str(i)
+                                })
+                        break
 
             out.write(str(protein_data)+"\n")
 
-sys.exit()
+    sys.exit()
